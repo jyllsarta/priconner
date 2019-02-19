d08a61618857c19d1620f033a56e63666d1c0f20
jyllsarta <illstarred.dive@gmail.com>
2019-02-19 23:11:09 +0900

    いったんforgesだけindexとshowを作成

--- app/controllers/forges_controller.rb
+++ app/controllers/forges_controller.rb
@@ -1,7 +1,10 @@
 class ForgesController < ApplicationController
     def index
+        @forge = Forge.new
+        @forges = Forge.all
     end
 
     def show
+        @forge = Forge.find_by(id: params[:id])
     end
 end
