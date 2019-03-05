f9081961bc3105876a24e7462cdae2890070306e
jyllsarta <illstarred.dive@gmail.com>
2019-03-06 00:15:47 +0900

    fix missing annotation

--- app/models/character.rb
+++ app/models/character.rb
@@ -6,8 +6,8 @@
 #  name           :string           default("0"), not null
 #  initial_rarity :integer          default(0), not null
 #  position       :integer          default(0), not null
-#  gw_image_id    :integer          default(0), not null
 #  gw_page_id     :integer          default(0), not null
+#  gw_image_id    :integer          default(0), not null
 #
 
 class Character < ApplicationRecord
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,6 @@
 #
 #  id          :integer          not null, primary key
 #  name        :string           default(""), not null
-#  is_material :boolean          default(FALSE), not null
 #  hp          :integer          default(0), not null
 #  atk         :integer          default(0), not null
 #  def         :integer          default(0), not null
@@ -21,6 +20,7 @@
 #  eva         :integer          default(0), not null
 #  drain       :integer          default(0), not null
 #  category    :integer          default(0), not null
+#  is_material :boolean          default(FALSE), not null
 #  gw_image_id :integer          default(0), not null
 #  gw_page_id  :integer          default(0), not null
 #
