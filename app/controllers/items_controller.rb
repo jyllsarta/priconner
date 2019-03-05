980a735ee7d89e5a688ccad43d3548afd64925bb
jyllsarta <illstarred.dive@gmail.com>
2019-03-06 00:12:54 +0900

    item, stageのindex, showアクセスを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,10 @@
 class ItemsController < ApplicationController
     def index
+        @item = Item.new
+        @items = Item.all
     end
 
     def show
+        @item = Item.find_by(id: params[:id])
     end
 end
--- app/controllers/stages_controller.rb
+++ app/controllers/stages_controller.rb
@@ -1,7 +1,10 @@
 class StagesController < ApplicationController
     def index
+        @stage = Stage.new
+        @stages = Stage.all
     end
 
     def show
+        @stage = Stage.find_by(id: params[:id])
     end
 end
