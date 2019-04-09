a0ceb917aaac9418c9aa95e2e906e43315e31990
jyllsarta <illstarred.dive@gmail.com>
2019-04-09 23:02:21 +0900

    落としてきたマスタをDBに突っ込む

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require "csv"
+
+EXPORT_TABLES = [:items, :forges, :drops, :stages, :equips, :characters]
+
+EXPORT_TABLES.map(&:to_s).each do |table_name|
+  clazz = table_name.singularize.camelize.constantize
+  clazz.delete_all
+  accepted_attributes = clazz.new.attributes.keys.map(&:to_sym)
+  CSV.open("masterdata/seeds/#{table_name}.csv", headers: true, header_converters: :symbol) do |csv|
+    csv.each do |line|
+      attributes = line.to_h.select{|k, v| k.in? accepted_attributes}
+      record = clazz.new(attributes)
+      record.save!(validate: false)
+    end
+  end
+  puts "created #{clazz} (#{clazz.count})"
+end
+
+puts "done!"
