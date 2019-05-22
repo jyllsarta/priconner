85716932c5b44c2cebf7db26c3d1f5cc5e9fd520
jyllsarta <illstarred.dive@gmail.com>
2019-05-22 22:39:23 +0900

    カテゴリソート追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,18 @@
 class ItemsController < ApplicationController
   def index
+    case params[:sort_key]&.to_sym
+    when :category
+      @items_sorted = Item.accumulate_by_key(:category)
+      @index_lambda = ->(x) {Item.category_texts.invert[x]}
+    else
+      @items_sorted = Item.accumulate_by_key(:rank)
+      @index_lambda = ->(x) {"rank #{x}"}
+    end
+  end
+
+  def by_category
     @item = Item.new
-    @items_sorted_by_rank = Item.accumulate_by_key(:rank)
+    @items_sorted_by_rank = Item.accumulate_by_key(:category)
   end
 
   def show
