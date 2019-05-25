8323005c7846ff8858d8f6d3df98761a99a4ef77
jyll sarta <illstarred.dive@gmail.com>
2019-05-25 22:34:22 +0900

    ほしいものリスト検索

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -1,11 +1,12 @@
 class WishlistsController < ApplicationController
   def index
-    @selected_item_ids = item_ids
+    @selected_items = item_ids.map{|item_id| Item.find_by(id: item_id)}.compact
     @items_sorted = Item.accumulate_by_key(:rank)
     @index_lambda = ->(x) {"rank #{x}"}
+    @serving_stages = Stage.serving_stages(@selected_items)
   end
 
   def item_ids
-    params[:item_ids]&.split(",")&.map(&:to_i).reject{|x| x==0} || []
+    params[:item_ids]&.split(",")&.map(&:to_i)&.reject{|x| x==0} || []
   end
 end
