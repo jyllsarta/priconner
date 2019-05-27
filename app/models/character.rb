916ab2778994434e6f407b0b08a2bac02e9c369a
jyllsarta <illstarred.dive@gmail.com>
2019-05-27 23:40:29 +0900

    キャラ詳細ページにランク比較を表示

--- app/models/character.rb
+++ app/models/character.rb
@@ -13,6 +13,29 @@ class Character < ApplicationRecord
   has_many :equips
   enum place: { front: 1, middle: 2, back: 3 }
 
+  def max_rank
+    equips.order(rank: :desc).first.rank
+  end
+
+  # ランクを上げた際のパラメータ差分
+  def rank_diffs(rank)
+    diffs = {}
+    # ぜんぜんDRYじゃないので直したいがそういう頭の回り方をする日じゃないので保留
+    # あとこれwherechainが更新されちゃってpreloadが効かなくなるのでN+1誘発しやすい書き方なのも良くない
+    equips.where(rank: rank).each do |equip|
+      equip&.item&.parameters&.map{|key, value| diffs[key] ||= 0; diffs[key] -= value}
+    end
+
+    equips.where("rank > #{rank}").where("rank < #{self.max_rank}").each do |equip|
+      equip&.item&.parameters&.map{|key, value| diffs[key] ||= 0; diffs[key] += value}
+    end
+
+    equips.where(rank: self.max_rank).each do |equip|
+      equip&.item&.parameters&.map{|key, value| diffs[key] ||= 0; diffs[key] += value * 2}
+    end
+    diffs
+  end
+
   def accumulate_equips_by_key(key)
     hash = {}
     self.equips.each do |item|
