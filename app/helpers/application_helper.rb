e240922b8bfdc92e3835b0820d2ceae57e40f441
jyllsarta <illstarred.dive@gmail.com>
2019-12-29 22:46:52 +0900

    railsからするとlocalhostはhttpなので、 ~~_url は httpを返してしまう

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,6 +11,6 @@ module ApplicationHelper
   def random_top_image_url
     # . と .. を除く簡単な方法ってあんのかな
     filename = Rails.root.join("public/images/top_illust").entries.reject{|e| e.to_s.starts_with?(".")}.map(&:to_s).sample
-    "#{root_url}images/top_illust/#{filename}"
+    "#{root_path}images/top_illust/#{filename}"
   end
 end
